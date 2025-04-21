c50751543cb54e790a323c7a89cb64cb837139bb
Radhika <radhika@xocortx.com>
2025-04-21 19:27:00 +0530

    contact us, price plan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AutoComplete\AutoCompleteController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Country\CountryController;
 use App\Http\Controllers\Customer\CustomerAdminController;
 use App\Http\Controllers\Customer\CustomerController;
@@ -53,7 +54,6 @@ Route::middleware('auth')->group(function () {
         ->name('workflow-module-authenticate');
     Route::patch('workflow-module-authenticate-update', [CustomerAdminController::class, 'workflowModuleAuthenticateUpdate'])
         ->name('workflow-module-authenticate-update');
-
 });
 // Sign Up Form
 Route::middleware('guest')->group(function () {
@@ -132,4 +132,11 @@ Route::middleware(['auth:customer'])->group(function () {
         ->name('customer-workflow-save');
 });
 
-require __DIR__.'/auth.php';
+//Nav Editor
+Route::resource('nav-editor', NavEditorController::class);
+Route::resource('footer-editor', FooterController::class);
+
+//contact form
+Route::post('send-contact-mail', [ContactController::class, 'sendMail']);
+
+require __DIR__ . '/auth.php';
