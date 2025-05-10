e494359b46eecb0c731f5a065566fe7bc9cdc03d
Radhika <radhika@xocortx.com>
2025-05-10 20:15:05 +0530

    customer verification id

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Geist', ...defaultTheme.fontFamily.sans],
+        'id-font': ['FragmentMono-Regular', ...defaultTheme.fontFamily.sans],
         'h1-stop': ['var(--font-h1-1stop)', ...defaultTheme.fontFamily.sans],
         'h2-1stop': ['var(--font-h2-1stop)', ...defaultTheme.fontFamily.sans],
         'h3-1stop': ['var(--font-h3-1stop)', ...defaultTheme.fontFamily.sans],
