528c3992692d62e3aa26aa7e72f0b613b361f6d3
Swaraj Krishnan <swarajkrishnan@outlook.com>
2025-06-20 11:57:24 +0530

    fix: correct index name when dropping unique constraint on telephone column

--- database/migrations/2025_06_20_061840_remove_teleplhone_unique_index.php
+++ database/migrations/2025_06_20_061840_remove_teleplhone_unique_index.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->dropUnique('telephone');
+            $table->dropUnique('customers_telephone_unique');
         });
     }
 
